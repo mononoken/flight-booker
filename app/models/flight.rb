40682f029fdd6b976d4bd52d704e199a69c71177
mononoken <ken.maeshima@pm.me>
2023-06-09 13:33:45 -0700

    Create scopes for distinct airport codes

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: "Airport", inverse_of: "departing_flights"
-  belongs_to :arrival_airport, class_name: "Airport", inverse_of: "arriving_flights"
+  belongs_to :departure_airport, class_name: 'Airport', inverse_of: 'departing_flights'
+  belongs_to :arrival_airport, class_name: 'Airport', inverse_of: 'arriving_flights'
+
+  scope :distinct_departure_airports, -> { joins(:departure_airport).distinct.order(:code).pluck(:code) }
+  scope :distinct_arrival_airports, -> { joins(:arrival_airport).distinct.order(:code).pluck(:code) }
 end
