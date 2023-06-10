1c2dc3ae9a4daadf3c4262c2131168b1ab0e0b3e
mononoken <ken.maeshima@pm.me>
2023-06-09 18:58:16 -0700

    Create distinct_depart_dates scope

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,15 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: 'Airport', inverse_of: 'departing_flights'
-  belongs_to :arrival_airport, class_name: 'Airport', inverse_of: 'arriving_flights'
+  belongs_to :departure_airport, class_name: "Airport", inverse_of: "departing_flights"
+  belongs_to :arrival_airport, class_name: "Airport", inverse_of: "arriving_flights"
 
-  scope :distinct_departure_airports, -> { joins(:departure_airport).distinct.order(:code).pluck(:code) }
-  scope :distinct_arrival_airports, -> { joins(:arrival_airport).distinct.order(:code).pluck(:code) }
+  scope :distinct_departure_airports,
+    -> { joins(:departure_airport).distinct.order(:code).pluck(:code) }
+  scope :distinct_arrival_airports,
+    -> { joins(:arrival_airport).distinct.order(:code).pluck(:code) }
+  scope :distinct_depart_dates,
+    lambda {
+      distinct.order(:start).pluck(:start).map do |start_datetime|
+        start_datetime.strftime("%F")
+      end
+    }
 end
