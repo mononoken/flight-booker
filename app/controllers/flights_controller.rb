3d687cffd1258c268e4d9484843c5ce8bdc20a57
mononoken <ken.maeshima@pm.me>
2023-06-13 19:04:39 -0700

    Create flight index view with HTML

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,29 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  def show
+    # @flight = Flight.find(params[:id])
+  end
+
+  def new
+    @flight = Flight.new
+  end
+
+  def create
+    @flight = Flight.new(flight_params)
+
+    if @flight.save
+      redirect_to @flight
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight)
+      .permit(:departure_airport_id, :arrival_airport_id, :start, :duartion)
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,9 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport", inverse_of: "departing_flights"
   belongs_to :arrival_airport, class_name: "Airport", inverse_of: "arriving_flights"
 
-  scope :distinct_departure_airports,
+  scope :departure_codes,
     -> { joins(:departure_airport).distinct.order(:code).pluck(:code) }
-  scope :distinct_arrival_airports,
+  scope :arrival_codes,
     -> { joins(:arrival_airport).distinct.order(:code).pluck(:code) }
   scope :distinct_depart_dates,
     lambda {
