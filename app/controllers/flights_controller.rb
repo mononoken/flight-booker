6a6f2210d1c35966e5b7cd5d4b4f6a96c0e76d22
mononoken <ken.maeshima@pm.me>
2023-06-16 21:06:23 -0700

    Create search functionality for flight and display results on flights#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,21 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @matching_flights = if params[:search].present?
+      Flight.search(params[:search])
+    else
+      @matching_flights = Flight.all
+    end
+
+    if turbo_frame_request?
+      render partial: "matching_flights", locals: {matching_flights: @matching_flights}
+    else
+      render :index
+    end
   end
 
   def show
-    # @flight = Flight.find(params[:id])
+    @flight = Flight.find(params[:id])
   end
 
   def new
@@ -25,6 +36,6 @@ class FlightsController < ApplicationController
 
   def flight_params
     params.require(:flight)
-      .permit(:departure_airport_id, :arrival_airport_id, :start, :duartion)
+      .permit(:departure_airport_id, :arrival_airport_id, :start, :duration)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,15 +1,25 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: "Airport", inverse_of: "departing_flights"
-  belongs_to :arrival_airport, class_name: "Airport", inverse_of: "arriving_flights"
+  def self.search(params)
+    where(
+      "start BETWEEN ? AND ?", params[:date].to_date.beginning_of_day, params[:date].to_date.end_of_day
+    )
+      .where(
+        departure_airport_id: Airport.find_by(code: params[:departure_code])&.id,
+        arrival_airport_id: Airport.find_by(code: params[:arrival_code])&.id
+      )
+  end
 
   scope :departure_codes,
     -> { joins(:departure_airport).distinct.order(:code).pluck(:code) }
   scope :arrival_codes,
     -> { joins(:arrival_airport).distinct.order(:code).pluck(:code) }
   scope :distinct_depart_dates,
-    lambda {
+    -> {
       distinct.order(:start).pluck(:start).map do |start_datetime|
         start_datetime.strftime("%F")
       end
     }
+
+  belongs_to :departure_airport, class_name: "Airport", inverse_of: "departing_flights"
+  belongs_to :arrival_airport, class_name: "Airport", inverse_of: "arriving_flights"
 end
