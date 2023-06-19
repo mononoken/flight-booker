936988aeb2044e903a60b330b67b1c3feb056015
mononoken <ken.maeshima@pm.me>
2023-06-18 22:58:18 -0700

    Create BookingsController and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   root "flights#index"
 
   resources :flights, only: %i[index]
+  resources :bookings, only: %i[new show index]
 end
