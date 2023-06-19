b38199cba3d63cb9cb2f78c654fb82b33e5cf229
mononoken <ken.maeshima@pm.me>
2023-06-18 22:53:10 -0700

    Setup relationships between, Flight, Passenger, and Booking

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -20,6 +20,8 @@ class Flight < ApplicationRecord
       end
     }
 
+  has_many :bookings
+  has_many :passengers, through: :bookings
   belongs_to :departure_airport, class_name: "Airport", inverse_of: "departing_flights"
   belongs_to :arrival_airport, class_name: "Airport", inverse_of: "arriving_flights"
 end
