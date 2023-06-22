4b5dddbb7668c928c5937ec1d26ce6ab68515aae
mononoken <ken.maeshima@pm.me>
2023-06-22 16:23:57 -0700

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-airports = %w[ATL AUS DEN DFW JFK LAX LGA ORD SEA SFO]
+Booking.delete_all
+Flight.delete_all
+Airport.delete_all
+
+airports = %w[ATL DFW JFK LAX LGA SEA SFO]
 airports.each do |code|
   Airport.create(code:)
 end
 
-10.times do
-  departure_airport_code = airports.sample
-  arrival_airport_code = (airports - [departure_airport_code]).sample
+# Generate 100 random flights for the next 30 days from today.
+(DateTime.now..DateTime.now + 30.days).to_a.each do |day|
+  100.times do
+    departure_airport_code = airports.sample
+    arrival_airport_code = (airports - [departure_airport_code]).sample
 
-  Flight.create(
-    departure_airport: Airport.find_by(code: departure_airport_code),
-    arrival_airport: Airport.find_by(code: arrival_airport_code),
-    start: rand(1.years).seconds.from_now,
-    duration: rand(6).hours + rand(59).minutes
-  )
+    Flight.create(
+      departure_airport: Airport.find_by(code: departure_airport_code),
+      arrival_airport: Airport.find_by(code: arrival_airport_code),
+      start: day + rand(1.days).seconds,
+      duration: rand(6).hours + rand(59).minutes # This duration is illogically randomized instead of calculated by distance.
+    )
+  end
 end
